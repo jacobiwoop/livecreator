566f8449b8bb1fe5ff1330f22d1904df977cf314
jacobiwoop <jacobiwoop@gmail.com>
2025-04-21 04:56:37 +0200

    Update controle.php

--- controle.php
+++ controle.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *"); // Permet l'accès depuis n'importe où (ou remplace * par un domaine précis)
