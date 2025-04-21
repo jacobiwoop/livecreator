2a65dfc0dfe7c992355cd89fd216f501f12effe0
jacobiwoop <jacobiwoop@gmail.com>
2025-04-21 04:51:41 +0200

    Update controle.php

--- controle.php
+++ controle.php
@@ -5,10 +5,7 @@ header("Access-Control-Allow-Origin: *"); // Permet l'accès depuis n'importe o�
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // ... le reste de ton code PHP
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
+
 require_once 'db.php'; // Connexion à la BDD
 
 $data = json_decode(file_get_contents('php://input'), true);
